329e02514ef10db6e2aa17be52e76105b31d955c
benergii <benweblin@gmail.com>
2025-03-12 18:01:02 +0000

    Ok we have left and right construction working suuuper well now (in concept lol). Still not rendering but we have orientation and direction of next cell working great.

--- src/functions/construction.py
+++ src/functions/construction.py
@@ -8,6 +8,14 @@ import config
 temp_cells_constructed_on = []
 temp_path = []
 
+# Might use this to decide directions for next construction block
+direction_mapper = [
+  (1, 0),
+  (0, 1),
+  (-1, 0),
+  (0, -1)
+]
+construction_direction = 0
 
 def place_first_piece_of_line():
 
@@ -35,8 +43,8 @@ def place_first_piece_of_line():
       (v4[1] + v1[1]) / 2
     ])
 
-    # Now dictate that the next cell being constructed on is x + 1 away from the interaction cell
-    config.construction_cell = tuple(add_vectors(config.interaction_cell, (1, 0)))
+    # Now dictate that the next cell being constructed on is x/y +- 1 away from the interaction cell
+    config.construction_cell = tuple(add_vectors(config.interaction_cell, direction_mapper[construction_direction]))
 
     # Let's see if any of that functionally worked...
     print(config.gameboard[config.interaction_cell])
@@ -47,22 +55,41 @@ def place_first_piece_of_line():
 
 
 # Pretty much the same pattern as above but with the construction_cell being used as reference rather than interaction_cell
-def place_next_piece_of_line():
+def place_next_piece_of_line(line_type, line_direction):
 
-  global temp_cells_constructed_on, temp_path
+  global temp_cells_constructed_on, temp_path, construction_direction
 
   if len(temp_cells_constructed_on) != 0:
 
+    # Line orientation == construction direction for left turns
+    # Line orientation == construction direction + 1 for right turns
+    # On the assumption that direction(0) = (1, 0) and rotates counterclockwise per each increment
+    # And orientation of the corner made by direction(0) -> direction(1) is also orientation(0), and also rotates counterclockwise
+    # Worst direction of that ever lmao, but draw it out on paper if you ever forget - that's how I just figured it out
+
+    if line_direction == 'left':
+      line_orientation = construction_direction
+      # Update the construction direction to reflect the direction provided in function
+      construction_direction = (construction_direction + 1) % 4
+    elif line_direction == 'right':
+      line_orientation = (1 + construction_direction) % 4
+      # Update the construction direction to reflect the direction provided in function
+      construction_direction = (construction_direction - 1) % 4
+    # else - drawing a straight line, which is trivially of orientation == direction
+    else:
+      line_orientation = construction_direction
+
     config.gameboard[config.construction_cell]['objectOnCell'] = {
-      'type': 'line',
-      'orientation': 1
+      'type': line_type,
+      'orientation': line_orientation
     }
 
     config.gameboard[config.construction_cell]['objectHeight'] = 0
 
     temp_cells_constructed_on.append(config.construction_cell)
 
-    config.construction_cell = tuple(add_vectors(config.construction_cell, [1, 0]))
+    # increment the construction cell to the next one
+    config.construction_cell = tuple(add_vectors(config.construction_cell, direction_mapper[construction_direction]))
 
     print(config.gameboard[config.construction_cell])
     print(temp_cells_constructed_on)
