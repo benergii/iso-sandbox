c7eb4a617fd8271250defa9a443533833483aede
benergii <benweblin@gmail.com>
2025-03-23 21:39:02 +0000

    Complete!

--- src/functions/terraform.py
+++ src/functions/terraform.py
@@ -1,6 +1,8 @@
 import config
 
-# WAS JUST USING THIS TO TEST THE GENERALISED POPUP BUTTON FUNCTIONALITY
+# TERRAFORM POPUP
+# Note that the below is following a generalised popup pattern, whereby the function name has to be 'mode'_popup(action)
+# This keeps all popup management very simple
 
 def terraform_popup(action):
 
@@ -9,3 +11,24 @@ def terraform_popup(action):
   elif action == 'decrease': config.terraform_scalar = max(config.terraform_scalar - 1, 0)
   
   print(f'Terraform Scalar is {config.terraform_scalar}')
+
+
+# The actual function to drag the cells up and down
+# Yeah we're modularising things baby!!
+
+def terraform_cells(units_dragged):
+
+  # First, let's store the min and max heights of the land being dragged
+  cell_heights = [config.gameboard[cell_index]['height'] for cell_index in config.interaction_cells]
+  min_cell_height = min(cell_heights); max_cell_height = max(cell_heights)
+
+  for cell_index in config.interaction_cells:
+
+    if (
+       # Positive units_dragged should only affect cells with height equal to the minimum height
+      (units_dragged > 0 and config.gameboard[cell_index]['height'] == min_cell_height)
+      or
+      # Conversely, negative units_dragged should do the same with maximum height``
+      (units_dragged < 0 and config.gameboard[cell_index]['height'] == max_cell_height)
+    ):
+        config.gameboard[cell_index]['height'] = max(config.gameboard[cell_index]['height'] + units_dragged, config.unit_height)
